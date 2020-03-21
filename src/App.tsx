83b74f2076b07b3d7bdb1a7b40646196948b8fab
Igor Souza <igorsouza.dev@gmail.com>
2020-03-21 19:05:49 -0300

    Adding login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div >
-      Hello World
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+      <div className="content">
+        <p>
+          Offer <strong>spots</strong> to programmers and find{' '}
+          <strong>talents</strong> for your company
+        </p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Your e-mail" />
+          <label htmlFor="password">PASSWORD *</label>
+          <input type="password" id="password" placeholder="Your password" />
+          <button type="submit" className="btn">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
